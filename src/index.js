b8fadee5d21004ba096ca272988347ca802b3095
Hussein Mohamed <hussein.abdallah96@eng-st.cu.edu.eg>
2021-08-02 14:45:30 +0200

    Added test cases

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-const express = require('express')
-require('./db/mongoose') // To run mongoose.js file to connect to database
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-// Parses incoming requests with JSON
-app.use(express.json())
-// register our Routers with the express app
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
